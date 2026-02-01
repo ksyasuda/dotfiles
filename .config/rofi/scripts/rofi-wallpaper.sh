e9e6dfa2d30246066ec865d8b5fc19d6a3fa9966
sudacode <suda@sudacode.com>
2026-02-01 12:20:52 -0800

    update

--- .config/rofi/scripts/rofi-wallpaper.sh
+++ .config/rofi/scripts/rofi-wallpaper.sh
@@ -4,7 +4,7 @@ set -Eeuo pipefail
 
 THEME="$HOME/.config/rofi/launchers/type-3/style-4.rasi"
 DIR="$HOME/Pictures/wallpapers/favorites"
-SELECTED_WALL=$(cd "$DIR" && for a in *.jpg *.png; do echo -en "$a\0icon\x1f$a\n"; done | rofi -dmenu -i -no-custom -theme "$THEME" -p "Select a wallpaper" -theme-str 'configuration {icon-size: 128; dpi: 96;} window {width: 45%; height: 45%;}')
+SELECTED_WALL=$(cd "$DIR" && for a in *.jpg *.png; do echo -en "$a\0icon\x1f$a\n"; done | rofi -dmenu -i -no-custom -theme "$THEME" -p "Select a wallpaper" -theme-str 'configuration {icon-size: 256; dpi: 96;} window {width: 75%; height: 69%;} listview {rows: 5; lines: 7;}')
 PTH="$(printf "%s" "$DIR/$SELECTED_WALL" | tr -s '/')"
 hyprctl hyprpaper wallpaper "DP-1, $PTH"
 notify-send -a "rofi-wallpaper" "Wallpaper set to" -i "$PTH" "$PTH"
