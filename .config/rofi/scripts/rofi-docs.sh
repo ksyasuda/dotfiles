ea550d8db40b423310d01bcacf08c50ca9c7a996
sudacode <suda@sudacode.com>
2026-03-29 23:12:15 -0700

    update

--- .config/rofi/scripts/rofi-docs.sh
+++ .config/rofi/scripts/rofi-docs.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 source "$SCRIPT_DIR/rofi-menu-helpers.sh"
 
-BROWSER=/usr/bin/zen-browser
+BROWSER=/usr/bin/helium-browser
 DOC_GROUPS=(
 	"Arch Linux (btw)|ARCH"
 	"Hyprland|HYPRLAND"
